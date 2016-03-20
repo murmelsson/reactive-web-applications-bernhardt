07886fe6c6daad45bbf6eca9dcebc65106c25d02
murmelssonic <dylan.drummond@gmail.com>
2016-03-21 01:01:22 +0200

    Ch2 p42 though not convinced it works right yet.

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
 # reactive-web-applications-bernhardt
 
+Using the Early-Release pdf copy, starting reading this 19.03.2016 (d-m-y).
+
 Note that I local-machine copy-paste conf/application.conf excluding things like secret API keys, into a file called 
 conf/application-dummy.conf, so that secret keys don't end up in a public repo.
 
 So if you want to use this repo, you will need to rename the dummy.conf to the standard name, and add your own keys etc.
 
 --
+# Using Play 2.5.0
+The book (#2.2.2) notes a bug in 2.4.x releases: https://github.com/playframework/playframework/pull/4826
+- but since in March 2016 we have in project/plugins.sbt :
+```
+// The Play plugin
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.5.0")
+```
+... so let's see if we can do without the workarounds also.
+
+Seems that the old-school file mentioned in section #2.2.3 app/controllers/Application.scala has been replaced with app/controllers/HomeController.scala
+
+So I am not going to switch back to Play 2.4.x, but it means there is going to be a side-project learning from this book,
+here is an example from Listing 2.3 Retrieval of the Configuration, when compiling:
+```
+[warn] /home/murmeister/playapps/twitter-stream/app/controllers/HomeController.scala:32: method configuration in object Play is deprecated: inject the play.api.Environment instead
+[warn]       apiKey <- Play.configuration.getString("twitter.apiKey")
+```
+
+
+
--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,8 +1,18 @@
 package controllers
 
 import javax.inject._
+
 import play.api._
 import play.api.mvc._
+import play.api.libs.oauth.{ConsumerKey, OAuthCalculator, RequestToken}
+import play.api.Play.current
+
+import scala.concurrent.Future
+import play.api.libs.concurrent.Execution.Implicits._
+import play.api.Environment._
+import play.api.libs.ws._
+import play.api.libs.iteratee._
+import play.api.Logger
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -12,13 +22,49 @@ import play.api.mvc._
 class HomeController @Inject() extends Controller {
 
   /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+    * Create an Action to render an HTML page with a welcome message.
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
+
+    def tweets = Action.async {
+
+      val loggingIteratee = Iteratee.foreach[Array[Byte]] { array =>
+        Logger.info(array.map(_.toChar).mkString)
+      }
+
+      credentials.map { case (consumerKey, requestToken) =>
+          WS
+            .url("https://stream.twitter.com/1.1/statuses/filter.json")
+            .sign(OAuthCalculator(consumerKey, requestToken))
+            .withQueryString("track" -> "reactive")
+            .get { response =>
+              Logger.info("Status: " + response.status)
+              loggingIteratee
+            }.map { _ =>
+              Ok("Stream closed")
+            }
+        } getOrElse {
+          Future {
+            InternalServerError("Twitter credentials missing")
+          }
+
+        }
+      }
+
+
+    def credentials: Option[(ConsumerKey, RequestToken)] = for {
+      apiKey <- Play.configuration.getString("twitter.apiKey")
+      apiSecret <- Play.configuration.getString("twitter.apiSecret")
+      token <- Play.configuration.getString("twitter.token")
+      tokenSecret <- Play.configuration.getString("twitter.tokenSecret")
+    } yield (
+      ConsumerKey(apiKey, apiSecret),
+      RequestToken(token, tokenSecret)
+      )
 
 }
