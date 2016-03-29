07bf102c0914ce24dd3073574a1894ae30f6ab1f
murmelssonic <dylan.drummond@gmail.com>
2016-03-30 01:09:41 +0300

    Get TweetReachComputer to compile...syntax ok but still WIP

--- app/actors/TweetReachComputer.scala
+++ app/actors/TweetReachComputer.scala
@@ -1,16 +1,101 @@
 package actors
 
-import akka.actor.{Actor, ActorRef, ActorLogging, Props}
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import akka.pattern.pipe
+import messages._
+import play.api.libs.json.JsArray
+import play.api.libs.oauth.OAuthCalculator
+import play.api.libs.ws.WS
 
-class TweetReachComputer(userFollowersCounter: ActorRef, storage: ActorRef) extends Actor with ActorLogging {
+import scala.concurrent.Future
+import scala.util.control.NonFatal
+//import play.api.Play.current
 
-  override def preStart(): Unit = log.info("hello, world (trc).")
+class TweetReachComputer(userFollowersCounter: ActorRef, storage: ActorRef) //1 ActorRefs as constructor parameters
+extends Actor with ActorLogging with TwitterCredentials {
+
+  implicit val executionContext = context.dispatcher   //2 use this.Actor's dispatcher as ec for executing Futures
+
+  var followerCountsByRetweet = Map.empty[FetchedRetweet, List[FollowerCount]]  //3 cache for currently computed follower-counts for a retweet
 
   def receive = {
-    case message =>   // do nothing
+
+    //val originalSender: ActorRef = sender() //in case of Future-failure, capture original sender (as sender may change in the meantime)
+    // val originalSender = sender() does not compile acc IntelliJ
+
+    case ComputeReach(tweetId) =>   //using pipe we avoid concurrent operations issue (race conditions):
+    //fetchRetweets(tweetId, sender()) pipeTo self   //4b - Pipe the fetchRetweets Future to this actor itself (original version without recover-path)
+    fetchRetweets(tweetId, sender()).recover {
+      case NonFatal(t) =>
+        //RetweetFetchingFailed(tweetId, t, originalSender)
+        RetweetFetchingFailed(tweetId, t, sender())
+
+    } pipeTo self
+
+    case fetchedRetweets: FetchedRetweet =>   //5b - Handle the received result of the Future
+      followerCountsByRetweet += fetchedRetweets -> List.empty
+      fetchedRetweets.retweeters.foreach { rt =>
+        userFollowersCounter ! FetchFollowerCount(fetchedRetweets.tweetId, rt)
+      }
+
+    case count @ FollowerCount(tweetId, _, _) =>
+    log.info("received followers-count for tweet {}", tweetId)
+    fetchedRetweetsFor(tweetId).foreach { fetchedRetweets =>
+      updateFollowersCount(tweetId, fetchedRetweets, count)
+    }
+
+    case ReachStored(tweetId) =>
+    followerCountsByRetweet.keys
+      .find(_.tweetId == tweetId)
+      .foreach { key =>
+        followerCountsByRetweet = followerCountsByRetweet.filterNot(_._1 == key)  //6 remove state after score has been stored
+      }
   }
 
+  case class FetchedRetweet(tweetId: BigInt, retweeters: List[BigInt], client: ActorRef)
+
+  case class RetweetFetchingFailed(tweetId: BigInt, cause: Throwable, client: ActorRef) //holds context of failure
+
+
+  def fetchedRetweetsFor(tweetId: BigInt) = followerCountsByRetweet.keys.find(_.tweetId == tweetId)
+
+  def updateFollowersCount(tweetId: BigInt, fetchedRetweets: FetchedRetweet, count: FollowerCount) = {
+    val existingCounts = followerCountsByRetweet(fetchedRetweets)
+    followerCountsByRetweet = followerCountsByRetweet.updated(fetchedRetweets, count :: existingCounts) //7 update state of retrieved follower-counts
+    val newCounts = followerCountsByRetweet(fetchedRetweets)
+
+    if (newCounts.length == fetchedRetweets.retweeters.length) {  //8 check if all follower counts were retrieved
+      log.info("Received all retweeters' followers-count for tweet {}, computing sum", tweetId)
+      val score = newCounts.map(_.followersCount).sum
+
+      fetchedRetweets.client ! TweetReach(tweetId, score)    //9 reply(tell) client the final score for tweet
+      storage ! StoreReach(tweetId, score)      //10 ask(tell) for tweet-score to be persisted
+    }
+  }
+
+  def fetchRetweets(tweetId: BigInt, client: ActorRef): Future[FetchedRetweet] = {    //11 fetch retweets TODO by using WS calls to Twitter API
+  // https://github.com/manuelbernhardt/reactive-web-applications/blob/master/CH06/app/actors/TweetReachComputer.scala
+
+    credentials.map {
+      case (consumerKey, requestToken) =>
+        WS.url("https://api.twitter.com/1.1/statuses/retweeters/ids.json")
+          .sign(OAuthCalculator(consumerKey, requestToken))
+          .withQueryString("id" -> tweetId.toString)
+          .withQueryString("stringify_ids" -> "true")
+          .get().map { response =>
+          if (response.status == 200) {
+            val ids = (response.json \ "ids").as[JsArray].value.map(v => BigInt(v.as[String])).toList
+            FetchedRetweet(tweetId, ids, client)
+          } else {
+            throw new RuntimeException(s"Could not retrieve details for Tweet $tweetId")
+          }
+        }
+    }.getOrElse {
+      Future.failed(new RuntimeException("You did not correctly configure the Twitter credentials"))
+    }
+  }
 }
+
 
 object TweetReachComputer {
   def props(followersCounter: ActorRef, storage: ActorRef) = 
---
+++ app/messages/Messages.scala
@@ -0,0 +1,10 @@
+package messages
+
+case class ComputeReach(tweetId: BigInt)
+case class TweetReach(tweetId: BigInt, score: Int)
+
+case class FetchFollowerCount(tweetId: BigInt, userId: BigInt)
+case class FollowerCount(tweetId: BigInt, userId: BigInt, followersCount: Int)
+
+case class StoreReach(tweetId: BigInt, score: Int)
+case class ReachStored(tweetId: BigInt)
